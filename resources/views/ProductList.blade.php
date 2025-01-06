709ef764198ee8d5b2560f643523e65c06a5f345
Mohamed Saad <sa3doni2714@gmail.com>
2025-01-06 19:31:54 +0200

    .

--- resources/views/ProductList.blade.php
+++ resources/views/ProductList.blade.php
@@ -93,16 +93,16 @@
     <table>
         <thead>
             <tr>
-                <th>الصورة</th>
-                <th>السعر</th>
                 <th>اسم المنتج</th>
+                <th>السعر</th>
+                <th>الصورة</th>
             </tr>
         </thead>
         <tbody>
             @foreach($show as $product)
             <tr>
-                <td>{{ $product->price }} ج.م</td>
                 <td>{{ $product->name }}</td>
+                <td>{{ $product->price }} ج.م</td>
                 <td><img src="{{ asset('images/' . $product->image) }}" alt="{{ $product->name }}"></td>
             </tr>
             @endforeach
